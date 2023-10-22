9d472a40075ce5ef52bac92a8cb566bd0f363aba
Mateusz Budzisz <electroluxv2@gmail.com>
2023-10-22 20:27:55 +0200

    Remove all files instead directory

--- Frontend/deploy.sh
+++ Frontend/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Removing all files from /var/www/city-planner.budziszm.pl/html/"
-rm -Rf /var/www/city-planner.budziszm.pl/html/
+echo "Removing all files from /var/www/city-planner.budziszm.pl/html"
+rm -Rf /var/www/city-planner.budziszm.pl/html/*
 
 echo "Copying dist files to /var/www/city-planner.budziszm.pl/html/"
 cp dist/frontend/* /var/www/city-planner.budziszm.pl/html/
