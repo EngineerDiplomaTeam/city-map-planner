a442b40556d927adbb1ae6ab9e68f03bf407595b
Mateusz Budzisz <electroluxv2@gmail.com>
2023-10-22 21:59:03 +0200

    Amazing

--- Backend/WebApi/Controllers/WeatherForecastController.cs
+++ Backend/WebApi/Controllers/WeatherForecastController.cs
@@ -7,7 +7,7 @@ namespace WebApi.Controllers;
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Amazing"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
