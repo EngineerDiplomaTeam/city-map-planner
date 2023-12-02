13edc131b184f9a1788a21bad14e3a3721a4212b
Mateusz Budzisz <ElectroluxV2@gmail.com>
2023-12-02 11:37:56 +0100

    Map bounds & sample polyline (#48)

--- Frontend/src/app/poi-selector/poi-selector.component.ts
+++ Frontend/src/app/poi-selector/poi-selector.component.ts
@@ -1,6 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  effect,
+  signal,
+} from '@angular/core';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import { MapOptions } from 'leaflet';
+import {
+  latLng,
+  latLngBounds,
+  MapOptions,
+  Map as LeafletMap,
+  polyline,
+} from 'leaflet';
 import { useColorSchemeBasedTileLayer } from './color-scheme-based-tile-layer';
 import { JsonPipe } from '@angular/common';
 
@@ -14,9 +25,35 @@ import { JsonPipe } from '@angular/common';
 })
 export class PoiSelectorComponent {
   protected readonly layerSignal = useColorSchemeBasedTileLayer();
+  protected readonly mapSignal = signal<LeafletMap | undefined>(undefined);
 
   protected readonly options: MapOptions = {
     zoom: 16,
     center: [54.3460523, 18.6559879],
+    maxBounds: latLngBounds(
+      latLng([54.448977, 18.450213]),
+      latLng([54.271424, 18.800024]),
+    ),
+    maxBoundsViscosity: 0.95,
+    maxZoom: 20,
+    minZoom: 13,
+    preferCanvas: true,
   };
+
+  constructor() {
+    effect(() => {
+      const m = this.mapSignal();
+      if (!m) return;
+
+      const l = polyline(
+        [[latLng([54.448977, 18.450213]), latLng([54.271424, 18.800024])]],
+        {
+          color: '#cddc39',
+          weight: 1,
+        },
+      );
+
+      m.addLayer(l);
+    });
+  }
 }
