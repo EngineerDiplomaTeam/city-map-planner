338bcbd9b2e98670e22b9d57db0e5bdb6e88d12a
Mateusz Budzisz <electroluxv2@gmail.com>
2024-04-14 21:37:45 +0200

    refactor pt 11

--- Frontend/src/app/poi/poi-basket-dialog/poi-basket-dialog.component.spec.ts
+++ Frontend/src/app/poi/poi-basket-dialog/poi-basket-dialog.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PoiBasketDialogComponent } from './poi-basket-dialog.component';
+import { provideMockStore } from '@ngrx/store/testing';
+import { selectPoisInBasket } from '../poi.selectors';
 
 describe('PoiBasketDialogComponent', () => {
   let component: PoiBasketDialogComponent;
@@ -9,6 +11,16 @@ describe('PoiBasketDialogComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [PoiBasketDialogComponent],
+      providers: [
+        provideMockStore({
+          selectors: [
+            {
+              selector: selectPoisInBasket,
+              value: [],
+            },
+          ],
+        }),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PoiBasketDialogComponent);
--- Frontend/src/app/poi/poi.selectors.ts
+++ Frontend/src/app/poi/poi.selectors.ts
@@ -2,7 +2,7 @@ import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { POI_FEATURE_KEY, PoiState } from './poi.reducer';
 import { OlMapMarker } from '../open-layers-map/ol-map-marker-manager.service';
 
-const selectPoiState = createFeatureSelector<PoiState>(POI_FEATURE_KEY);
+export const selectPoiState = createFeatureSelector<PoiState>(POI_FEATURE_KEY);
 
 export const selectPoiOpenedInBottomSheet = createSelector(
   selectPoiState,
