a2a598875bf712ce5a3f4feff5a4c11ecfb45954
Wiktor <wiktor.rostkowski@hotmail.com>
2024-04-18 22:23:28 +0200

    Improve Version

---
+++ Backend/WebApi.Weatherx/Hourly.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApi.Weather;
+public class Hourly
+    {
+        public string[]? Time { get; set; } 
+        public int?[]? Weathercode { get; set; }
+
+    }
---
+++ Backend/WebApi.Weatherx/WeatherModel.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace WebApi.Weather;
+
+public class WeatherModel
+{
+    public string Id { get; set; }
+    public float Latitude { get; set; }
+    public float Longitude { get; set; }
+    public string Timezone { get; set; }
+    public string? Weathercode { get; set; }
+    public HourlyOptions Hourly { get { return _hourly; } set { if (value != null) _hourly = value; } }
+    public WeatherModelOptions Models { get { return _models; } set { if (value != null) _models = value; } }
+    public string Start_date { get; set; }
+    public string End_date { get; set; }
+    private HourlyOptions _hourly = new HourlyOptions();
+    private WeatherModelOptions _models = new WeatherModelOptions(); // to delete
+
+    
+    public WeatherModel(float latitude, float longitude, string timezone, HourlyOptions hourly,int past_Days, string start_date, string end_date, WeatherModelOptions models)
+    {
+        Latitude = latitude;
+        Longitude = longitude;
+        Timezone = timezone;
+
+        if (hourly != null)
+            Hourly = hourly;
+        if (models != null)
+            Models = models;
+        Start_date = start_date;
+        End_date = end_date;
+    }
+
+}
+
+
+
+
