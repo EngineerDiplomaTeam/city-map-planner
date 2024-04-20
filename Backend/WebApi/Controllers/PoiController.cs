54d8e1272160af48db8ed91b3e8a714225d006b8
Mateusz Budzisz <electroluxv2@gmail.com>
2024-04-21 01:36:36 +0200

    Authorize

--- Backend/WebApi/Controllers/PoiController.cs
+++ Backend/WebApi/Controllers/PoiController.cs
@@ -1,4 +1,5 @@
 using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApi.Data;
@@ -9,7 +10,7 @@ using WebApi.Services;
 
 namespace WebApi.Controllers;
 
-[ApiController]
+[ApiController, Authorize(Roles = "Administrator")]
 [Route("[controller]")]
 public class PoiController(IPoisManagerService poisManagerService) : ControllerBase
 {
