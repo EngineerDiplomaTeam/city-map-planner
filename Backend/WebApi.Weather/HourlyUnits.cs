f405efaf56074e41ceec8ecdede0dbe20694b793
Wiktor <wiktor.rostkowski@hotmail.com>
2024-05-02 16:04:47 +0200

    Update missing PascalCase in file

--- Backend/WebApi.Weather/HourlyUnits.cs
+++ Backend/WebApi.Weather/HourlyUnits.cs
@@ -1,12 +1,16 @@
-﻿namespace WebApi.Weather;
+﻿using System.Text.Json.Serialization;
+
+namespace WebApi.Weather;
 
 public class HourlyUnits
 {
     public string? Time { get; set; }
-    public string? Temperature_2m { get; set; }
+    [JsonPropertyName("Temperature_2m")]
+    public string? Temperature2m { get; set; }
 
     public string? Weathercode { get; set; }
 
     // New Weather models API
-    public string? Weathercode_best_match { get; set; }
+    [JsonPropertyName("Weathercode_best_matc")]
+    public string? WeathercodeBestMatch { get; set; }
 }
