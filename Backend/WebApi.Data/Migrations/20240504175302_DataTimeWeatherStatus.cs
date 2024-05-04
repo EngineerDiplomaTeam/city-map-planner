7ac3bfdac9c4225ac967c347cccc34d4ed749ff2
Wiktor <wiktor.rostkowski@hotmail.com>
2024-05-04 20:27:59 +0200

    Add Migration

---
+++ Backend/WebApi.Data/Migrations/20240504175302_DataTimeWeatherStatus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataTimeWeatherStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "time",
+                schema: "data",
+                table: "weather_status",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "time",
+                schema: "data",
+                table: "weather_status",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+    }
+}
