d3f69996ab2b492315990f4c69010608ba536ec0
Mateusz Budzisz <electroluxv2@gmail.com>
2024-05-10 14:34:18 +0200

    Fix migration

---
+++ Backend/WebApi.Data/Migrations/DataDb/20240510123337_AddWeatherData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace WebApi.Data.Migrations.DataDb
+{
+    /// <inheritdoc />
+    public partial class AddWeatherData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "weather_status",
+                schema: "data",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    weather_code = table.Column<int>(type: "integer", nullable: false),
+                    temperature = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_weather_status", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "weather_status",
+                schema: "data");
+        }
+    }
+}
