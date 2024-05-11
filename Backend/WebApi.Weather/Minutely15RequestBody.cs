1f899291a34701bfa61bfc0b39fd558b628bb966
Wiktor Rostkowski <wiktor.rostkowski@hotmail.com>
2024-05-11 15:04:53 +0200

    Weather API (#92)
    Co-authored-by: Mateusz Budzisz <electroluxv2@gmail.com>

---
+++ Backend/WebApi.Weather/Minutely15RequestBody.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApi.Weather;
+
+public class Minutely15ResponseBody
+{
+    public string[]? Time { get; set; } = null!;
+    public int[] Weathercode { get; set; } = null!;
+
+    [JsonPropertyName("temperature_2m")] public double[] Temperature2M { get; set; } = null!;
+}
---
+++ Backend/WebApi/Services/WeatherUpdater.cs
@@ -0,0 +1,100 @@
+using WebApi.Domain;
+using WebApi.Weather;
+using static System.Convert;
+
+namespace WebApi.Services;
+
+public class WeatherUpdater(
+    IWeatherClient weatherClient,
+    ILogger<WeatherUpdater> logger,
+    IServiceProvider serviceProvider)
+    : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var lastUpdate = DateTime.MinValue;
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            if (DateTime.Now - lastUpdate < TimeSpan.FromDays(1))
+            {
+                await Task.Delay(TimeSpan.FromSeconds(10));
+                continue;
+            }
+
+            lastUpdate = DateTime.Now;
+            await DoUpdate(stoppingToken);
+        }
+    }
+
+    private async Task DoUpdate(CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Updating WeatherCode for next 3  days");
+
+        using var scope = serviceProvider.CreateScope();
+        var weatherService = scope.ServiceProvider.GetRequiredService<IWeatherService>();
+        var weatherAll = await weatherService.GetAllWeatherStatusAsync().ToListAsync(cancellationToken);
+
+        var thisDay = DateTime.Today;
+        var theDayAfterTomorrow = DateTime.Today.AddDays(2);
+        var formatDateForOpenMeteo = "yyyy-MM-dd";
+
+        var dateQueryStart = thisDay.ToString(formatDateForOpenMeteo);
+        var dateQueryEnd = theDayAfterTomorrow.ToString(formatDateForOpenMeteo); // Example Weather for three days
+
+
+        var optionsApi = new WeatherForecastOptionsApi();
+        optionsApi.Latitude = 54.34f; // For Gdansk
+        optionsApi.Longitude = 18.64f; // For Gdansk
+        optionsApi.StartDate = dateQueryStart;
+        optionsApi.EndDate = dateQueryEnd;
+        optionsApi.Timezone = "Europe/Warsaw";
+
+        optionsApi.Minutely15.Add(Minutely15OptionsParameter.weathercode);
+        optionsApi.Minutely15.Add(Minutely15OptionsParameter.temperature_2m);
+
+
+        // Api call to get the current weather in Gdansk
+        var weatherData = await weatherClient.GetWeatherForecastAsync(optionsApi);
+
+        WeatherStatus? update;
+
+
+        // Weather Database is null
+        if (weatherAll.Count == 0)
+        {
+            if (weatherData != null && weatherData.Minutely15 != null)
+                for (var i = 0; i < weatherData.Minutely15.Weathercode.Length; i++)
+                    if (weatherData.Minutely15.Time != null)
+                    {
+                        var id = i + 1;
+                        update = new WeatherStatus(id, ToDateTime(weatherData.Minutely15.Time[i]).ToUniversalTime(),
+                            weatherData.Minutely15.Weathercode[i],
+                            ToDouble(weatherData.Minutely15.Temperature2M[i]));
+                        logger.LogInformation("Add new weatherCode");
+
+                        await weatherService.AddWeatherStatusAsync(update, cancellationToken);
+                    }
+        }
+        else
+        {
+            if (weatherData != null && weatherData.Minutely15 != null)
+                for (var i = 0; i < weatherData.Minutely15.Weathercode.Length; i++)
+                {
+                    var x = weatherAll.Find(info =>
+                        weatherData.Minutely15.Time != null &&
+                        info.Time == ToDateTime(weatherData.Minutely15.Time[i]).ToUniversalTime());
+                    if (x == null)
+                        if (weatherData.Minutely15.Time != null)
+                        {
+                            update = new WeatherStatus(weatherAll.Count + i + 1,
+                                ToDateTime(weatherData.Minutely15.Time[i]).ToUniversalTime(),
+                                weatherData.Minutely15.Weathercode[i], weatherData.Minutely15.Temperature2M[i]);
+                            logger.LogInformation("Add new weatherCode");
+                            await weatherService.AddWeatherStatusAsync(update, cancellationToken);
+                        }
+                }
+            else
+                logger.LogError("Weather data is unavailable.");
+        }
+    }
+}
